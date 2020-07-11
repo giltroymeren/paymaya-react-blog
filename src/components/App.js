c8fae5ff397501fc42b4608ab151b677fbd0f484
Giltroy Meren <giltroymeren@gmail.com>
2020-07-11 17:52:24 +0800

    Setup routing for manage post page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import PostsPage from './posts/PostsPage';
+import ManagePostPage from './posts/ManagePostPage';
 
 const App = () => {
     return (
-        <>
+        <div className="container-fluid">
             <h1>PayMaya React App</h1>
-            <PostsPage />
-        </>
+            <Switch>
+                <Route path="/posts" component={PostsPage} />
+                <Route path="/post/:slug" component={ManagePostPage} />
+                <Route path="/post" component={ManagePostPage} />
+            </Switch>
+        </div>
     );
 };
 
