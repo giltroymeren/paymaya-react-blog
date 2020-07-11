f5b22506fcb5498ed6b94f064813ce60c39ae60d
Giltroy Meren <giltroymeren@gmail.com>
2020-07-11 11:23:00 +0800

    Configured babel

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import { render } from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const App = () => <p>Hello World</p>;
-
 render(
     <App />,
     document.getElementById("app")
