f372f9d8fc1e04b8d03e347298b2191b3d139cac
Giltroy Meren <giltroymeren@gmail.com>
2020-07-12 18:09:44 +0800

    Added notice for no posts

--- src/components/posts/PostsList.js
+++ src/components/posts/PostsList.js
@@ -7,31 +7,41 @@ import './PostsList.css';
 
 const PostsList = ({ posts, onDelete }) => {
     return (
-        <PaginationList
-            data={posts}
-            pageSize={5}
-            renderItem={post => (
-                <div key={post.slug} className="row">
-                    <div className="col">
-                        <Link to={`/post/${post.slug}`}>{post.title}</Link>
-                    </div>
-                    <div className="col-1">
-                        <Link
-                            to={`/post/edit/${post.slug}`}
-                            className="btn btn-outline-primary">
-                            EDIT
-                        </Link>
-                    </div>
-                    <div className="col-2">
-                        <button
-                            className="btn btn-outline-danger"
-                            onClick={() => onDelete(post)}>
-                            DELETE
-                        </button>
-                    </div>
+        <>
+        { posts.length > 0
+            ? (
+                <PaginationList
+                    data={posts}
+                    pageSize={5}
+                    renderItem={post => (
+                        <div key={post.slug} className="row">
+                            <div className="col">
+                                <Link to={`/post/${post.slug}`}>{post.title}</Link>
+                            </div>
+                            <div className="col-1">
+                                <Link
+                                    to={`/post/edit/${post.slug}`}
+                                    className="btn btn-outline-primary">
+                                    EDIT
+                                </Link>
+                            </div>
+                            <div className="col-2">
+                                <button
+                                    className="btn btn-outline-danger"
+                                    onClick={() => onDelete(post)}>
+                                    DELETE
+                                </button>
+                            </div>
+                        </div>
+                    )}
+                />
+            )
+            : (
+                <div className="alert alert-secondary" role="alert">
+                    No posts available.
                 </div>
             )}
-        />
+        </>
     );
 }
 
